b2b72a27e5ecc4a8c11816684053620dbe6dc4cd
pedrcosta <pedrcosta@live.com.pt>
2017-02-03 20:40:44 +0000

    new files added

---
+++ js/preload.js
@@ -0,0 +1,32 @@
+/*!
+ * JavaScript preload() function – http://mths.be/preload
+ * Preload images, CSS and JavaScript files without executing them
+ * Script by Stoyan Stefanov – http://www.phpied.com/preload-cssjavascript-without-execution/
+ * Slightly rewritten by Mathias Bynens – http://mathiasbynens.be/
+ */
+
+function preload(array) {
+	var length = array.length,
+	    document = window.document,
+	    body = document.body,
+	    isIE = 'fileSize' in document,
+	    object;
+	while (length--) {
+		if (isIE) {
+			new Image().src = array[length];
+			continue;
+		}
+		object = document.createElement('object');
+		object.data = array[length];
+		object.width = object.height = 0;
+		body.appendChild(object);
+	}
+}
+
+// Example:
+
+preload([
+	'http://hang.nodester.com/foo.png?2000',
+	'http://hang.nodester.com/foo.js?2000',
+	'http://hang.nodester.com/foo.css?2000'
+]);
